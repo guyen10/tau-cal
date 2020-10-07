2e18a34ab75d323fbcbd30846a4bc5a3a2117c11
Guy Matzliach <guyen10@gmail.com>
2020-10-07 21:46:31 +0300

    Adding comments on code, update manifast version

--- js/btn.js
+++ js/btn.js
@@ -7,7 +7,7 @@ function addButton(){
     (document.head || document.documentElement).appendChild(s);
     let t = $("table[bordercolor='skyblue']")[0]
     let th = t.rows[0].cells[t.rows[0].cells.length-1]
-    th.innerHTML = '<button id="down_btn" style="font-size: 16px;background-color: #f44336;border-radius: 8px;font-weight: bold" type="button" onclick="download_calendar();">יצא מערכת לקובץ↓</button>'
+    th.innerHTML = '<button id="down_btn" style="font-size: 16px;background-color: #f44336;border-radius: 8px;font-weight: bold" type="button" onclick="download_calendar();">יצא מערכת<br/> לקובץ↓</button>'
 
 
 }
